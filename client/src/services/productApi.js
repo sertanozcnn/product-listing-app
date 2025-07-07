b129c67f65eaf74bbb79545c43a5c4671b603dc2
Sertan <sertanozcan.tr@gmail.com>
2025-07-07 17:15:09 +0300

    Every

--- client/src/services/productApi.js
+++ client/src/services/productApi.js
@@ -1,8 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl =
-  process.env.REACT_APP_BACKEND_API_URL ||
-  "https://productapi-4lmzhf5w.b4a.run/api";
+const baseUrl = "https://productapi-4lmzhf5w.b4a.run/api";
 
 export const productApi = createApi({
   reducerPath: "productApi",
