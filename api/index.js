3ee8026d312debb1519e9cce8f3c995529babe60
Sertan <sertanozcan.tr@gmail.com>
2025-07-07 17:22:32 +0300

    Latest Updated

--- api/index.js
+++ api/index.js
@@ -6,28 +6,23 @@ const productRoutes = require("./src/routes/productRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(express.json());
-
-app.use(cors());
-app.use("/api/products", productRoutes);
-
-// Routes
-//app.use("/api/products", productRoutes);
-
-// Ana route
-
-// Server başlatma
 const corsOptions = {
   origin: "*", // veya sadece frontend URL'si (örneğin: "https://yourfrontend.com")
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // Eğer kimlik doğrulama varsa
   allowedHeaders: ["Content-Type", "Authorization"],
 };
+
+// Middleware
+app.use(express.json());
 app.use(cors(corsOptions));
+
+app.use("/api/products", productRoutes);
+
 app.get("/", (req, res) => {
   res.send("Product API is running");
 });
+
 app.listen(PORT, () => {
   console.log(`API running at http://localhost:${PORT}`);
 });
